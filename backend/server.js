0e564b15490f57ca60ecff86ed9a7f6ef6f641a9
Tiago Gomes <tidgomes@gmail.com>
2023-08-03 13:48:19 -0300

    feat: enable cors in the backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import connectToDB from './config/db.js'
 import router from './routes/router.js'
 import errorHandler, {
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000
 dotenv.config()
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 
